f293047397937ea3910902627b171040242b13e5
Samson <samson.asefa@tobania.be>
2023-12-06 15:44:25 +0300

    Initial Commit

---
+++ Emojis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emojis
+//
+//  Created by samsona on 12/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emojis/EmojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojisApp.swift
+//  Emojis
+//
+//  Created by samsona on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
