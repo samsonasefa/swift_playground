af7d595331d5af958f2ff7b708794981ed826266
Samson <samson.asefa@tobania.be>
2023-12-08 13:47:10 +0300

    first commit

--- Emojis/ContentView.swift
+++ Emojis/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+enum Emoji:String,CaseIterable{
+    case 👇, 😂, 🎉, 🚀
+}
 struct ContentView: View {
+    
+    @State var selection: Emoji = .🎉
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size:150))
+                
+                Picker("Select Emoji",selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self){ emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji")
             .padding()
+            
+        }
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
